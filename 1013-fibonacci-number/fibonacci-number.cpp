7cbe8c4955032b0228501c45fc0fdf298593fe57
nvarshitha27 <1nc23cs172@ncetmail.com>
2026-04-27 17:56:58 +0530

    Time: 8 ms (48.68%) | Memory: 7.6 MB (88.26%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int fib(int n) {
+        
+        if(n <= 1) return n;
+        return fib(n-1) + fib(n-2);
+    }
+    int main(){
+        int n;
+        cin >> n;
+     cout << fib(n);
+     return 0;
+    }
+};
+ 
