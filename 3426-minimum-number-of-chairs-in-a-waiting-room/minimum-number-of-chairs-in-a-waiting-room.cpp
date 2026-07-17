9dc8ce4d942829d1de8748a4e82f9213f21633dd
nvarshitha27 <1nc23cs172@ncetmail.com>
2026-07-17 11:00:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (58.50%) - LeetSync

--- 3426-minimum-number-of-chairs-in-a-waiting-room/minimum-number-of-chairs-in-a-waiting-room.cpp
+++ 3426-minimum-number-of-chairs-in-a-waiting-room/minimum-number-of-chairs-in-a-waiting-room.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int minimumChairs(string s) {
-      int p=0,c=0;
+      int c=0,m=0;
       for(int i = 0; i < s.size(); i++){
         if(s[i] == 'E'){
-           p++;
-           if(p>c){
-            c=p;
-           }
-        }else{
-            p--;
+            c++;
+            m=max(m,c);
+        }else {
+            c--;
         }
-      } 
-      return c; 
+      }
+      return m;
     }
 };
